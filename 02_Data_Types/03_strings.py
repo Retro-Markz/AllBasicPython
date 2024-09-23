14e77c5243c1f940f891a42663e9febbd900f062
Retro-Markz <cubedomarco@gmail.com>
2024-09-23 12:06:34 -0700

    String update

--- 02_Data_Types/03_strings.py
+++ 02_Data_Types/03_strings.py
@@ -147,3 +147,79 @@ print(a.replace("H", "J"))
 a = "Hello, World!"
 print(a.split(",")) # returns ['Hello', ' World!']
 
+# String Concatenation
+# To concatenate, or combine, two strings you can use the + operator.
+
+
+a = "Hello"
+b = "World"
+c = a + b
+print(c)
+
+a = "Hello"
+b = "World"
+c = a + " " + b
+print(c)
+
+
+# String Format
+
+# As we learned in the Python Variables chapter, we cannot combine strings and numbers like this:
+
+age = 36
+txt = "My name is John, I am " , age
+print(txt)
+
+# But we can combine strings and numbers by using f-strings or the format() method!
+
+# F-Strings
+# F-String was introduced in Python 3.6, and is now the preferred way of formatting strings.
+
+# To specify a string as an f-string, simply put an f in front of the string literal, and add curly brackets {} as placeholders for variables and other operations.
+
+age = 36
+txt = f"My name is John, I am {age}"
+print(txt)
+
+# Placeholders and Modifiers
+# A placeholder can contain variables, operations, functions, and modifiers to format the value.
+
+price = 59
+txt = f"The price is {price} dollars"
+print(txt)
+
+# A placeholder can include a modifier to format the value.
+
+# A modifier is included by adding a colon : followed by a legal formatting type, like .2f which means fixed point number with 2 decimals:
+
+
+price = 59
+txt = f"The price is {price:.2f} dollars"
+print(txt)
+
+# A placeholder can contain Python code, like math operations:
+
+txt = f"The price is {20 * 59} dollars"
+print(txt)
+
+
+# Escape Character\
+    
+# To insert characters that are illegal in a string, use an escape character.
+
+# An escape character is a backslash \ followed by the character you want to insert.
+
+# An example of an illegal character is a double quote inside a string that is surrounded by double quotes:
+
+
+# You will get an error if you use double quotes inside a string that is surrounded by double quotes:
+
+#txt = "We are the so-called "Vikings" from the north."
+
+# To fix this problem, use the escape character \":
+
+txt = "We are the so-called \"Vikings\" from the north."
+
+print(txt)
+
+
