ff92891c0c9a28fe4856c69fec1e6fade70f0f03
Retro-Markz <cubedomarco@gmail.com>
2024-09-23 12:43:58 -0700

    add operators

---
+++ 03_Operators/operators.py
@@ -0,0 +1,43 @@
+# Python Operators
+# Operators are used to perform operations on variables and values.
+
+# In the example below, we use the + operator to add together two values:
+
+
+
+'''
+Python divides the operators in the following groups:
+
+Arithmetic operators
+Assignment operators
+Comparison operators
+Logical operators
+Identity operators
+Membership operators
+Bitwise operators
+
+'''
+
+# Operator Precedence
+
+# Operator precedence describes the order in which operations are performed.
+
+# Parentheses has the highest precedence, meaning that expressions inside parentheses must be evaluated first:
+
+print((6 + 3) - (6 + 3))
+
+# Multiplication * has higher precedence than addition +, and therefor multiplications are evaluated before additions:
+
+print(100 + 5 * 3)
+
+# Addition + and subtraction - has the same precedence, and therefor we evaluate the expression from left to right:
+
+print(5 + 4 - 7 + 3)
+    
+    
+    
+    
+    
+    
+    
+    
