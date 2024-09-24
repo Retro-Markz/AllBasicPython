a1e6156187a04dd93bc086e4262e65de6eb2e0ad
Retro-Markz <cubedomarco@gmail.com>
2024-09-23 18:59:32 -0700

    making some practice with the special characters to format text

---
+++ 02_Data_Types/02_1_Working_with_strings/working_with_Strings.py
@@ -0,0 +1,43 @@
+
+#special characters
+
+print()
+
+a = 'it\'s a new day'
+
+print(a)
+print()
+
+b = 'in the new message \\ just select the backslash'
+
+print(b)
+print()
+
+c = 'it\'s easy when you practice, \nand everybody need to practice'
+
+print(c)
+print()
+
+d = 'well let\'s see what\'s  \r  a carrierbarrier does'
+print(d)
+print()
+
+e = '\t adding a tab is easy \nthe best part it\'s to apply it'
+print(e)
+print()
+
+a = "what\'s a \bBackspace? \nreally don\'t know"
+print(a)
+print()
+
+a = 'form feed \f seem\'s new to me\n\tlet\'s try it'
+print(a)
+print()
+
+a = '''let\'s try form feed\f
+    Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse efficitur ipsum sit amet ex vestibulum finibus. Praesent eu placerat quam. Ut pellentesque vestibulum orci, ac interdum dolor ornare id. Mauris posuere mollis lacus eget vestibulum. Mauris finibus augue id ante accumsan, et sollicitudin purus aliquet. Maecenas rhoncus vulputate urna, vel suscipit diam varius vitae. Donec molestie lacus et imperdiet faucibus. Donec eu porta elit. Nam bibendum dui urna, vitae tempor purus egestas a. Donec tempus volutpat lorem, vel venenatis sapien fermentum nec. Pellentesque eu venenatis est, quis ornare velit. Proin eget magna ac sapien dapibus convallis finibus quis turpis. Integer justo lacus, aliquam at hendrerit eget, feugiat et nunc.
+'''
+
+print(a)
+print()
+
