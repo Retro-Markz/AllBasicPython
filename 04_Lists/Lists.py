5a806d1ce5c0e1ad24c97c0cc1cd209b1a6a785a
Retro-Markz <cubedomarco@gmail.com>
2024-09-24 01:05:10 -0700

    starting with lists

---
+++ 04_Lists/Lists.py
@@ -0,0 +1,20 @@
+print()
+mylist = ["apple", "banana", "cherry"]
+
+print(mylist[1])
+print()
+# List
+
+# Lists are used to store multiple items in a single variable.
+# Lists are one of 4 built-in data types in Python used to store collections of data, the other 3 are Tuple, Set, and Dictionary, all with different qualities and usage.
+# Lists are created using square brackets:
+
+thislist = ["apple", "banana", "cherry"]
+print(thislist)
+print()
+
+# Ordered
+
+# When we say that lists are ordered, it means that the items have a defined order, and that order will not change.
+# If you add new items to a list, the new items will be placed at the end of the list.
+
