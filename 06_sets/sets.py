46ce76927bbe8cf1d8097179fc4b0bc6420f6fb8
Retro-Markz <cubedomarco@gmail.com>
2024-09-26 17:09:38 -0700

    set done

--- 06_sets/sets.py
+++ 06_sets/sets.py
@@ -356,14 +356,59 @@ print(set3)
 print()
 
 # Use - to join two sets:
+# Note: The - operator only allows you to join sets with sets, and not with other data types like you can with the difference() method.
 
 set1 = {"apple", "banana", "cherry"}
 set2 = {"google", "microsoft", "apple"}
 
 set3 = set1 - set2
+print(f'set 3: {set3}')
+
+print()
+
+# Use the difference_update() method to keep the items that are not present in both sets:
+
+set1 = {"apple", "banana", "cherry"}
+set2 = {"google", "microsoft", "apple"}
+
+set1.difference_update(set2)
+
+print(f'difference_update: {set1}')
+
+print()
+
+# The symmetric_difference() method will keep only the elements that are NOT present in both sets.
+
+
+set1 = {"apple", "banana", "cherry"}
+set2 = {"google", "microsoft", "apple"}
+
+set3 = set1.symmetric_difference(set2)
+
 print(set3)
 
 print()
 
+# You can use the ^ operator instead of the symmetric_difference() method, and you will get the same result.
 
+
+set1 = {"apple", "banana", "cherry"}
+set2 = {"google", "microsoft", "apple"}
+
+set3 = set1 ^ set2
+print(f'using the ^ : {set3}')
+
+# Note: The ^ operator only allows you to join sets with sets, and not with other data types like you can with the symmetric_difference() method.
+
+# The symmetric_difference_update() method will also keep all but the duplicates, but it will change the original set instead of returning a new set.
+
+
+# Use the symmetric_difference_update() method to keep the items that are not present in both sets:
+
+set1 = {"apple", "banana", "cherry"}
+set2 = {"google", "microsoft", "apple"}
+
+set1.symmetric_difference_update(set2)
+
+print(set1)
 
