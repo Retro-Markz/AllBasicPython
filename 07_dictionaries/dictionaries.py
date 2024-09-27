da025c1cceb9c5cafc524f203b66f452ab8c4be5
Retro-Markz <cubedomarco@gmail.com>
2024-09-26 17:11:30 -0700

    start dictionaries

---
+++ 07_dictionaries/dictionaries.py
@@ -0,0 +1,29 @@
+# Dictionary
+
+# Dictionaries are used to store data values in key:value pairs.
+
+# A dictionary is a collection which is ordered*, changeable and do not allow duplicates.
+
+print()
+
+thisdict = {
+  "brand": "Ford",
+  "model": "Mustang",
+  "year": 1964
+}
+
+# Dictionaries are written with curly brackets, and have keys and values:
+
+thisdict = {
+  "brand": "Ford",
+  "model": "Mustang",
+  "year": 1964
+}
+print(thisdict)
+
+print()
+
+# Dictionary items are ordered, changeable, and do not allow duplicates.
+
+# Dictionary items are presented in key:value pairs, and can be referred to by using the key name.
+
